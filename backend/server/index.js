2dc4e71933a881c1d879f0ba3d2a3b05a15f8032
RDM_ <rafadiazm77@gmail.com>
2022-07-30 11:59:26 -0500

    add 404 route w/wild operator

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,10 +1,30 @@
-let http = require('http')
-let server = http.createServer(function (req, res) {
-    res.write('Hello FSD!')
-    res.end()
+require('dotenv').config()
+const express = require("express")
+const app = express()
+
+
+
+
+// Homepage route
+
+app.get('/', (res,req)=>{
+    console.log('this is my home page')
 })
 
-server.listen(3000, function () {
-    console.log('I am awake!')
+// Login route
+
+app.get('/login', (res,req)=>{
+    console.log('this is my login page')
 })
 
+
+
+// wild operator route
+
+app.get('*', (res,req)=>{
+    res.statusCode(404).send("<h1>404 Page</h1>")
+    
+    
+})
+
+app.listen(process.env.PORT, () => console.log('awaken from express index js'))
