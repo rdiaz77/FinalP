0c36159e3ebf27f3fab32e5a61f052b6a4955a42
RDM_ <rafadiazm77@gmail.com>
2022-07-30 20:24:22 -0500

    I will create the models folder. All was working until this point

---
+++ backend/server/models/places.js
@@ -0,0 +1,21 @@
+module.exports =  [{
+    name: 'Clinica Alemana',
+    type : 'Private',
+    address : "Av. Vitacura 9000",
+    county: " Vitacura",
+    city: "Santiago",
+    region: "Metropolitana",
+    mainContact : "John Doe",
+    mainContactPhone: "555-555-555"
+
+}, {
+    name: 'Hospital Padre Hurtado',
+    type : 'Public',
+    address : "Santa Rosa 10000",
+    county: " Pudahuel",
+    city: "Santiago",
+    region: "Metropolitana",
+    mainContact : "Henry Doe",
+    mainContactPhone: "555-555-9999"
+
+}]
