4addeba26dfb9ff5a4b63df400eafa38446e8997
RDM_ <rafadiazm77@gmail.com>
2022-08-01 14:07:10 -0500

    connected to postgres

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,14 +1,33 @@
+// DEPENDENCIES
 const methodOverride = require('method-override');
 require('dotenv').config()
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT
+const { Sequelize } = require('sequelize')
+
+
+
+/// SEQUELIZE CONNECTION
+
+const sequelize = new Sequelize('SVTrace', 'postgres', "Asturias171#", {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
 
 
 
 // MIDDLEWARE (express)-- allows the use of JSX for server-side rendering (SSR) in Express 
-// link the ./controllers/places file with the main index file
 
+// link the ./controllers/places file with the main index file
 
 app.use(methodOverride("_method"))
 app.set('view engine', 'jsx')
@@ -16,7 +35,10 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
-// View routes
+
+
+// VIEW routes
+
 app.use('/places',require('./controllers/places'))
 app.use('/contacts', require('./controllers/contacts'))
 app.use('/users', require('./controllers/users'))
@@ -45,4 +67,8 @@ app.get('*', (req,res)=>{
     
 })
 
+//LISTEN port
+
 app.listen(PORT, () => console.log('awaken from express index js at port', PORT))
+
+
